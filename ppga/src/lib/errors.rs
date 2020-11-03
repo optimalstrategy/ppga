4bae15eb99a16a8219aef081d0164b8ae1ffd4f6
OptimalStrategy <17456182+OptimalStrategy@users.noreply.github.com>
2020-11-03 14:49:14 -0500

    add docs

--- ppga/src/lib/errors.rs
+++ ppga/src/lib/errors.rs
@@ -1,19 +1,28 @@
+//! This module defines the error handling and reporting facilities used by the transpiler.
 use logos::Span;
 
+/// A span with a line.
 #[derive(Debug, Clone)]
 pub struct LineSpan {
+    /// The start of the span as an offset from the start of the source code.
     pub start: usize,
+    /// The end of the span as an offset from the start of the source code.
     pub end: usize,
+    /// The line this span starts on.
     pub line: usize,
 }
 
+/// A parse error.
 #[derive(Debug, Clone)]
 pub struct ParseError {
+    /// The span of the token that produces this error.
     pub span: LineSpan,
+    /// The error message.
     pub message: String,
 }
 
 impl ParseError {
+    /// Create a new parse error.
     pub fn new<S: Into<String>>(line: usize, span: Span, message: S) -> ParseError {
         Self {
             span: LineSpan {
@@ -25,6 +34,9 @@ impl ParseError {
         }
     }
 
+    /// Create a new parse error from a [`LineSpan`].
+    ///
+    /// [`LineSpan`]: crate::errors::LineSpan
     pub fn with_linespan<S: Into<String>>(span: LineSpan, message: S) -> ParseError {
         Self {
             span,
@@ -33,14 +45,21 @@ impl ParseError {
     }
 }
 
+/// The error context used for collecting and reporting errors.
 #[derive(Debug, Clone)]
 pub struct ErrCtx<'a> {
+    /// The recorded errors.
     pub errors: Vec<ParseError>,
+    /// The source code the errors originate from.
     pub source: &'a str,
+    /// The source code split by `\n`.
     pub lines: Vec<&'a str>,
 }
 
 impl<'a> ErrCtx<'a> {
+    /// Creates a new [`ErrCtx`] from the given PPGA source.
+    ///
+    /// [`ErrCtx`]: struct.ErrCtx
     pub fn new(source: &'a str) -> Self {
         Self {
             errors: vec![],
@@ -49,19 +68,25 @@ impl<'a> ErrCtx<'a> {
         }
     }
 
+    /// Records a [`ParseError`].
+    ///
+    /// [`ParseError`]: struct.ParseError
     pub fn record(&mut self, e: ParseError) {
         self.errors.push(e)
     }
 
+    /// Returns `true` if one or more errors have been recorded.
     #[inline(always)]
     pub fn had_error(&self) -> bool {
         !self.errors.is_empty()
     }
 
+    /// Reports all errors to STDERR.
     pub fn report_all(&self) {
         eprintln!("{}", self.report_to_string());
     }
 
+    /// Reports all errors to a String.
     pub fn report_to_string(&self) -> String {
         let mut result = vec![];
 
--- ppga/src/lib/lib.rs
+++ ppga/src/lib/lib.rs
@@ -1,3 +1,21 @@
+//! # PPGA Script
+//! PPGA Script is a scripting language that transpiles to Lua. It
+//! provides a more familiar C-style syntax and syntactic sugar,
+//! designed to reduce the amount of boilerplate when writing scripting commands in lua.
+//!
+//! ## Usage
+//! ```rust,ignore
+//! extern crate ppga;
+//!
+//! fn main() {
+//!     let source = load_file(...);
+//!     let result = ppga::ppga_to_lua(&source, ppga::PPGAConfig::default());
+//!     match result {
+//!         Ok(lua) => println!("{}", lua),
+//!         Err(ex) => eprintln!("{}", ex.report_to_string()),
+//!     }
+//! }
+//! ```
 #![feature(box_patterns)]
 extern crate logos;
 
@@ -15,6 +33,18 @@ use frontend::{lexer, Parser};
 pub use codegen::code_builder::DEFAULT_INDENT_SIZE;
 pub use config::PPGAConfig;
 
+/// Transpiles the given PPGA source to Lua. The returned value is either the resulting Lua code or an [`ErrCtx`] with the errors.
+///
+/// ```rust
+/// # extern crate ppga;
+/// # use ppga::{ppga_to_lua, PPGAConfig};
+/// assert_eq!(
+///     ppga_to_lua("let a = 2;", PPGAConfig::default().disable_std()).unwrap(),
+///     "local a = 2"
+/// );
+/// ```
+///
+///[`ErrCtx`]: crate::errors::ErrCtx
 pub fn ppga_to_lua(source: &str, config: PPGAConfig) -> Result<String, ErrCtx<'_>> {
     Parser::with_config(config, lexer(source.trim_end()))
         .parse()
